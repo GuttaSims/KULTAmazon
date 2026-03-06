a10f9fa45df0e28a1a066a877ae50eefe900540c
GuttaSims <45774506+GuttaSims@users.noreply.github.com>
2026-03-05 22:36:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-document.getElementById("registerForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("registerForm").addEventListener("submit", async function(e){
 
-    alert("Registration successful!");
-});
+e.preventDefault()
+
+const sl_name = document.getElementById("sl_name").value
+const uuid = document.getElementById("uuid").value
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const res = await fetch("/api/register",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+sl_name,
+uuid,
+email,
+password
+})
+})
+
+const data = await res.json()
+
+document.getElementById("message").innerText = data.message
+
+})
