b035f60e22901c929bf7551820b3ef7e245de26e
GuttaSims <45774506+GuttaSims@users.noreply.github.com>
2026-03-06 21:43:01 -0500

    Update get-products.js

--- api/get-products.js
+++ api/get-products.js
@@ -1,28 +1,36 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabase = createClient(
-process.env.SUPABASE_URL,
-process.env.SUPABASE_SERVICE_ROLE
-)
-
 export default async function handler(req, res) {
 
 try {
 
+const supabase = createClient(
+process.env.SUPABASE_URL,
+process.env.SUPABASE_SERVICE_ROLE_KEY
+)
+
+const { vendor } = req.query
+
 const { data, error } = await supabase
 .from("products")
 .select("*")
+.eq("vendor_username", vendor)
 
-if (error) {
-return res.status(500).json(error)
+if(error){
+console.error(error)
+return res.status(500).json({ products: [] })
 }
 
-res.status(200).json(data)
+return res.json({
+products: data || []
+})
 
-} catch (err) {
+} catch(err){
 
-res.status(500).json({
-error: err.message
+console.error(err)
+
+return res.status(500).json({
+products: []
 })
 
 }
