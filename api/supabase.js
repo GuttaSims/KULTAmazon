76ca1b408767714b88dae54bf6c8ce54e2370346
GuttaSims <45774506+GuttaSims@users.noreply.github.com>
2026-03-07 15:07:42 -0500

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 export default supabase;
