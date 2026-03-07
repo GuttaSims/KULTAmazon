513aa26441d0fc826a6869862a5c416d84a7c1e5
GuttaSims <45774506+GuttaSims@users.noreply.github.com>
2026-03-06 20:05:54 -0500

    Update get-products.js

--- api/get-products.js
+++ api/get-products.js
@@ -5,25 +5,25 @@ export default async function handler(req, res) {
 try {
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
+process.env.SUPABASE_URL,
+process.env.SUPABASE_SERVICE_ROLE
 )
 
 const { data, error } = await supabase
-  .from("products")
-  .select("*")
+.from("products")
+.select("*")
 
 if (error) {
-  return res.status(500).json({ error: error.message })
+return res.status(500).json({ error: error.message })
 }
 
-return res.status(200).json(data)
+res.status(200).json(data)
 
 } catch (err) {
 
-return res.status(500).json({
-  error: "Server crashed",
-  details: err.message
+res.status(500).json({
+error: "Server crashed",
+details: err.message
 })
 
 }
