4b14f476ccdec3d97a2563f0193dc188294d7718
GuttaSims <45774506+GuttaSims@users.noreply.github.com>
2026-03-06 21:40:33 -0500

    Update get-vendors.js

--- api/get-vendors.js
+++ api/get-vendors.js
@@ -6,7 +6,7 @@ try {
 
 const supabase = createClient(
 process.env.SUPABASE_URL,
-process.env.SUPABASE_SERVICE_ROLE
+process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
 const { data, error } = await supabase
